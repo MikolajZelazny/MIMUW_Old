be01d4bb50b91898d88713473f158659bf88cfcb
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-02-08 19:33:01 +0100

    Add person structure

--- MIMUW Tasks/PersonLibrary/src/Main.java
+++ MIMUW Tasks/PersonLibrary/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
    /* Napisz program składający się z dwu klas, umieszczonych w dwu plikach (ale w jednym pakiecie).
-    Jeden plik ma zawierać definicję klasy Osoba (z imieniem i nazwiskiem oraz metodami pozwalającymi
+    Jeden plik ma zawierać definicję klasy Person (z imieniem i nazwiskiem oraz metodami pozwalającymi
     na odczytywanie i zapisywanie tych atrybutów oraz konstruktorem),
     drugi ma wczytać od użytkownika dane 10 osób, zapamiętać je w tablicy osób,
     a następnie wypisać w odwrotnej kolejności.
