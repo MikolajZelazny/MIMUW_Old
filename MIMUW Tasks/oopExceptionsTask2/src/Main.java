4ca68216f9628042377083685483224ebc2982ca
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-02-23 21:39:17 +0100

    task 2 completed

--- MIMUW Tasks/oopExceptionsTask2/src/Main.java
+++ MIMUW Tasks/oopExceptionsTask2/src/Main.java
@@ -1,16 +1,29 @@
 
+
 public class Main {
     class excA extends Exception {}
     class excB extends Exception  {}
     class excC extends Exception  {}
 
     void rndmExc() throws excA, excB, excC {
+        double x = (int)(Math.random()*3)+1;
 
+        if (x == 1.0) throw new excA();
+        else if (x == 2.0) throw new excB();
+        else throw new excC();
     }
 
 
     public static void main(String[] args) throws Exception{
-
-        //void randomException
+        Main obj = new Main();
+        try {
+            obj.rndmExc();
+        } catch (excA e) {
+            System.out.println(e.getClass());
+        } catch (excB e) {
+            System.out.println(e.getClass());
+        }// catch (excC e) {
+           // System.out.println(e.getClass());
+        //}
     }
 }
