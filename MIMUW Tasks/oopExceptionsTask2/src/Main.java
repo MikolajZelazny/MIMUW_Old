f83698ba8dcdf13436082739c0b61a9d56374a3f
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-02-23 19:48:40 +0100

    task 2 init

---
+++ MIMUW Tasks/oopExceptionsTask2/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    class excA extends Exception {}
+    class excB extends Exception  {}
+    class excC extends Exception  {}
+
+    void rndmExc() throws excA, excB, excC {
+
+    }
+
+
+    public static void main(String[] args) throws Exception{
+
+
+
+        //void randomException
+    }
+}
