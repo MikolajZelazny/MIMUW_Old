b6f548aa299416e0a4284437308e2a9b6f7a44e7
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-02-23 19:27:49 +0100

    init

--- MIMUW Tasks/oopExceptionsTask1/src/Main.java
+++ MIMUW Tasks/oopExceptionsTask1/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public void strToLength (String a) {
+            System.out.println(a.length());
+        }
+
+    public static void main(String[] args) throws Exception{
+        try {
+            Main obj = new Main();
+            obj.strToLength(null);
+        } catch (NullPointerException n) {
+            n.printStackTrace(System.out);
+            //n.fillInStackTrace(); - Temporary
+            //throw n; - Temporary
+            throw new Exception(); // Same usage as above method implementation.
+        } /*finally {
+            //System.out.println("Final!");
+        }*/
     }
 }
