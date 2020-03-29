52659d2622deb2f10120e9c78dcc6f9ac7ae1bb8
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-03-29 20:48:22 +0200

    code for upgrade

---
+++ MIMUW Tasks/oopExceptionsTask7 task6Upgrade/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+class InnyMożliwyWyjątek extends Exception {}
+
+class Zasób3 {
+    Zasób3(int i) {
+        //...
+    }
+    void zarezerwuj() {}
+    void używaj() throws IOException {}
+    void innaNiebezpiecznaOperacja() throws InnyMożliwyWyjątek {}
+    void zwolnij() {}
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            Zasób3 z = new Zasób3(i);
+            try {
+                z.zarezerwuj();
+
+                //tu jest niebezpieczny kod
+                z.używaj();
+                z.innaNiebezpiecznaOperacja();
+                if (i == 3) continue;
+                if (i == 8) return;
+                //if (i == 8) break;
+                //...
+
+            } catch (IOException e) {
+                //obsługa wyjątku IOException
+            } catch (InnyMożliwyWyjątek w) {
+                //obsługa wyjątku InnyMożliwyWyjątek
+            } finally {
+                //zwalnianie zasobów
+                z.zwolnij();
+            }
+        }
+    }
+}
