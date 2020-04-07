9752627367c2c5158c0ac6587b626bd702f8c597
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-04-07 17:35:21 +0200

    task complete

--- MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/Main.java
+++ MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/Main.java
@@ -1,7 +1,12 @@
 
-public class Main {
 
-    public static void main(String[] args) {
+public class Main  {
 
+    public static void main(String[] args) throws Exception{
+        try {
+            UnsafeConstructor uc = new UnsafeConstructor();
+        } catch (Exception e) {
+            System.out.println("\n" + e.toString() + " catched. Task complete!");
+        }
     }
 }
--- MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/UnsafeConstructor.java
+++ MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/UnsafeConstructor.java
@@ -1,6 +1,6 @@
 
 
-public class UnsafeConstructor {
+public class UnsafeConstructor   {
     public UnsafeConstructor() throws Exception { // adding exception to method signature
         throw new Exception();
     }
