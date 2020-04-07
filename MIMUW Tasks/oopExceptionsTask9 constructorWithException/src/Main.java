104eae6dc9b266017703d39a0ba5e76851e6bdaa
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-04-07 17:25:45 +0200

    todo added

--- MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/Main.java
+++ MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/Main.java
@@ -1,3 +1,6 @@
+// TODO Napisz klasę, której konstruktor zgłasza wyjątek.
+//  W metodzie main() utwórz egzemplarz tej klasy i obsłuż wyjątek.
+
 public class Main {
 
     public static void main(String[] args) {
