675737c265bc98beb00bb88f7f41d347944ac0db
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-04-07 17:27:25 +0200

    adding exception to method signature

--- MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/Main.java
+++ MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/Main.java
@@ -1,5 +1,3 @@
-// TODO Napisz klasę, której konstruktor zgłasza wyjątek.
-//  W metodzie main() utwórz egzemplarz tej klasy i obsłuż wyjątek.
 
 public class Main {
 
--- MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/UnsafeConstructor.java
+++ MIMUW Tasks/oopExceptionsTask9 constructorWithException/src/UnsafeConstructor.java
@@ -1,2 +1,8 @@
+
+
 public class UnsafeConstructor {
+    public UnsafeConstructor() throws Exception { // adding exception to method signature
+        throw new Exception();
+    }
+
 }
