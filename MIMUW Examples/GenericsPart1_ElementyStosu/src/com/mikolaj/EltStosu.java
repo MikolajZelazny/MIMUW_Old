e23d4999cdf3243991f6a4e1a21f621130147c1e
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-05-12 00:18:46 +0200

    antother class added2

--- MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/EltStosu.java
+++ MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/EltStosu.java
@@ -1,4 +1,17 @@
 package com.mikolaj;
 
-public class EltStosu {
+class EltStosu {
+
+    public final Object elt;
+    public final EltStosu nast;
+    // Ponieważ obiekty klasy EltStosu są ukryte w Stosie, nie
+    // ma potrzeby deklarowania ich atrybutów jako prywatnych.
+    // Również fakt, że klasa EltStosu nie jest publiczna, oraz deklaracja
+    // final pozwalają tu na wyjątek od ogólnej reguły, zgodnie z którą
+    // atrybuty zawsze powinny by prywatne lub chronione.
+
+    public EltStosu(Object elt, EltStosu nast) {
+        this.elt = elt;
+        this.nast = nast;
+    }
 }
