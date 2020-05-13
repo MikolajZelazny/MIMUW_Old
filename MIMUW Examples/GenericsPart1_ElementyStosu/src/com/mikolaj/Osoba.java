12f17ae6b768701e4bbbef7e5ae69f7a18a9e443
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-05-13 18:20:55 +0200

    class Osoba

---
+++ MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Osoba.java
@@ -0,0 +1,13 @@
+package com.mikolaj;
+
+public class Osoba {
+    private String imię;
+
+    public Osoba(String imię) {
+        this.imię = imię;
+    }
+
+    public String imię(){
+        return imię;
+    }
+}
