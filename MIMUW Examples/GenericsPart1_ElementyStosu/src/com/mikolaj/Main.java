e022ea5e4c0d16af06ff59e42e71c2468e6fbb6f
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-05-15 23:04:39 +0200

    exception and second code implementation variant added

--- MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Main.java
+++ MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Main.java
@@ -2,11 +2,12 @@ package com.mikolaj;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PustyStos {
         Osoba os1 = new Osoba("Jasio");
         Stos s = new Stos();
 
         s.wstaw(os1);
        // Osoba os2 = s.pobierz(); // metoda pobierz ma typ wyniku Object, a my potrzebujemy obiektu klasy Osoba.
+       // Osoba os2 = (Osoba) s.pobierz();
     }
 }
