710fb64cd86ad221d062f1a8863019a8b6963278
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-05-15 23:02:11 +0200

    code implementation and explanation of the problematic use of Object super class

--- MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Main.java
+++ MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Main.java
@@ -3,6 +3,10 @@ package com.mikolaj;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Osoba os1 = new Osoba("Jasio");
+        Stos s = new Stos();
+
+        s.wstaw(os1);
+       // Osoba os2 = s.pobierz(); // metoda pobierz ma typ wyniku Object, a my potrzebujemy obiektu klasy Osoba.
     }
 }
