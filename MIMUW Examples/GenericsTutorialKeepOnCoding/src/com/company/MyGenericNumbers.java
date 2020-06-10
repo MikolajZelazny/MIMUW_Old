bd57a00f310ac26922d54be4771564c8fe5e87bc
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-06-10 23:24:00 +0200

    class body terminology clarification

--- MIMUW Examples/GenericsTutorialKeepOnCoding/src/com/company/MyGenericNumbers.java
+++ MIMUW Examples/GenericsTutorialKeepOnCoding/src/com/company/MyGenericNumbers.java
@@ -3,14 +3,14 @@ package com.company;
 public class MyGenericNumbers<T extends Number> {
     //Number --> Integer, Double Float
 
-    T ob;
+    T num;
 
     MyGenericNumbers(T ob) {
-        this.ob = ob;
+        this.num = ob;
     }
 
     double square (){
-        return ob.intValue() * ob.doubleValue(); //without "extends Number" compiler doesn't know type so this is error
+        return num.intValue() * num.doubleValue(); //without "extends Number" compiler doesn't know type so this is error
         // because we cannon't multiply string * string.
     }
 }
