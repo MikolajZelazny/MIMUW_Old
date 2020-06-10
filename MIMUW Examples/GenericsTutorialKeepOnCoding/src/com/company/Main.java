95fc868f866d66b459cbfb6558a731d25d0bd069
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-06-10 23:17:06 +0200

    square method body and implementation

--- MIMUW Examples/GenericsTutorialKeepOnCoding/src/com/company/Main.java
+++ MIMUW Examples/GenericsTutorialKeepOnCoding/src/com/company/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         MyGenericClass<Integer, String, Integer> obj = new MyGenericClass<>(10, "razy", 10);
-
         obj.showType();
+
+        MyGenericNumbers<Integer> obj2 = new MyGenericNumbers<>(8);
+        System.out.println(obj2.square());
+
     }
 }
--- MIMUW Examples/GenericsTutorialKeepOnCoding/src/com/company/MyGenericNumbers.java
+++ MIMUW Examples/GenericsTutorialKeepOnCoding/src/com/company/MyGenericNumbers.java
@@ -8,7 +8,7 @@ public class MyGenericNumbers<T extends Number> {
         this.ob = ob;
     }
 
-    double square (T ob){
+    double square (){
         return ob.intValue() * ob.doubleValue(); //without "extends Number" compiler doesn't know type so this is error
         // because we cannon't multiply string * string.
     }
