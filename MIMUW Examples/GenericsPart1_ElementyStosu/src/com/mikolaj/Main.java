6cf61416eb2aa60785ff66a019c07451ffd05aca
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-06-13 23:41:45 +0200

    test true for null

--- MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Main.java
+++ MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Main.java
@@ -13,8 +13,8 @@ public class Main {
         System.out.println("Równość par: " +
                 new Para<Integer, Integer>(0,0).equals(new Para<Integer, Integer>(0,0)));
 
-        Para p1 = new Para(8, "Stru");
-        Para p2 = new Para(8, "Stru");
+        Para p1 = new Para(null, "Stru");
+        Para p2 = new Para(null, "Stru");
         System.out.println("Test2: " + p1.equals(p2));
 
     }
