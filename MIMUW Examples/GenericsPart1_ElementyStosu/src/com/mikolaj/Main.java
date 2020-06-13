10d213464e6ac16a7c1b2140f5c8b57ac7527b4e
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-06-13 23:28:26 +0200

    test of second way to use equal

--- MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Main.java
+++ MIMUW Examples/GenericsPart1_ElementyStosu/src/com/mikolaj/Main.java
@@ -12,5 +12,13 @@ public class Main {
 
         System.out.println("Równość par: " +
                 new Para<Integer, Integer>(0,0).equals(new Para<Integer, Integer>(0,0)));
+
+        Para p1 = new Para(8, "Stru");
+        Para p2 = new Para(8, "Stru");
+        System.out.println("Test2: " + p1.equals(p2));
+
     }
+
+
+
 }
